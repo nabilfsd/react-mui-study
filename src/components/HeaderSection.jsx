2d7f5fe11e3b864a34f9cc4d4052eed846eada43
nabilfsd <nabil.the.fsd@gmail.com>
2022-01-26 23:14:39 +0600

    feat: materual ui drawer

--- src/components/HeaderSection.jsx
+++ src/components/HeaderSection.jsx
@@ -1,29 +1,81 @@
-import React from 'react';
-import { Box, AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Drawer,
+  List,
+  ListItem,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const HeaderSection = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+    console.log("drawer open", open);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+    console.log("drawer closed");
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ margin: '0px'}}>
+      <AppBar position='static' sx={{ margin: "0px" }}>
         <Toolbar>
           <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
             sx={{ mr: 2 }}
+            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
-    </Box>
-  )
-}
 
-export default HeaderSection
+      <Drawer
+        sx={{
+          width: "300px",
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: "300px",
+            boxSizing: "border-box",
+          },
+        }}
+        variant='temporary'
+        open={open}
+      >
+        <Box>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+            sx={{ mr: 2 }}
+            onClick={handleDrawerClose}
+          >
+            <MenuIcon />
+          </IconButton>
+          <List>
+            <ListItem onClick={() => console.log("list 1 clicked")}>1</ListItem>
+            <ListItem onClick={() => console.log("list 1 clicked")}>2</ListItem>
+          </List>
+        </Box>
+      </Drawer>
+    </Box>
+  );
+};
+
+export default HeaderSection;
